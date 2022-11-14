ef7e0e2c34ba7553042b435a236d732c8f0fffb0
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-14 11:57:09 +0530

    Initial Commit

---
+++ R-Card App/R_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_Card_AppApp.swift
+//  R-Card App
+//
+//  Created by Singh, Akash | RIEPL on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct R_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
